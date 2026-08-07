88bf4828ac35e8eddf73c3b08c1d5c2aa2713387
Олег Чижов <oleh.chyzhov@gmail.com>
2026-08-07 22:02:16 +0300

    feat: initial db setup

---
+++ Airbnb.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Airbnb.Infrastructure;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) {}
+}
